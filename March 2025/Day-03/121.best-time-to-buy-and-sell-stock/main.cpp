8a987f62a7fad8c2dd9861ff104aad06cfb284fc
tailsmonster <aroca.twelve@proton.me>
2025-05-03 11:24:25 -0400

    added leetcode 3

---
+++ March 2025/Day-03/121.best-time-to-buy-and-sell-stock/main.cpp
@@ -0,0 +1,16 @@
+cpp
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int bestbuy = prices[0];
+        int maxprofit = 0;
+        for(int i = 1; i<prices.size(); i++){
+            if(prices[i] > bestbuy){
+                maxprofit = max(maxprofit, prices[i] - bestbuy);
+            }
+            bestbuy = min(bestbuy, prices[i]);
+        }
+        return maxprofit;
+    }
+};
